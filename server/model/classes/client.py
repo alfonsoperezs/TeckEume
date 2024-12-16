0bfff2b19c598757b50581beeba27f53a6dbd54a
alfonsoperezs <fonpesan@gmail.com>
2024-12-16 14:52:28 +0100

    [SERVER] Changed directories

---
+++ server/model/classes/client.py
@@ -0,0 +1,9 @@
+class Client():
+    def __init__(self, id: int, name: str, email: str, phone_number: str, assigned_employee: int, password: str):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.phone_number = phone_number
+        self.assigned_employee = assigned_employee
+        self.password = password
+        
